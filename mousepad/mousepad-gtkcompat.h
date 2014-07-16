48f37d339b9d2157cfca5a28aa62f3e93151901e
Matthew Brush <mbrush@codebrainz.ca>
2014-07-16 00:14:20 -0700

    Build with GSEAL_ENABLE defined
    Sadly this disables the (semi-buggy but still useful) vertical
    selection since I have absolutely no idea how this code works and
    it uses some GtkTextView internals. The code is still active if not
    building with either GTK3 or GSEAL_ENABLE.
    
    Another thing is extending the hacks related to adding our GtkUiManager
    menu as the GtkTextView popup menu. Instead of directly replacing
    GtkTextView's internal menu pointer, empty its menu and move our own
    items there from the UI manager menu.

--- mousepad/mousepad-gtkcompat.h
+++ mousepad/mousepad-gtkcompat.h
@@ -8,4 +8,23 @@
 # define gtk_combo_box_text_new            gtk_combo_box_new_text
 #endif
 
+#if ! GTK_CHECK_VERSION(3, 0, 0)
+static inline gint
+gtk_widget_get_allocated_width (GtkWidget *widget)
+{
+  GtkAllocation alloc = { 0, 0, 0, 0 };
+  g_return_val_if_fail (GTK_IS_WIDGET (widget), -1);
+  gtk_widget_get_allocation (widget, &alloc);
+  return alloc.width;
+}
+static inline gint
+gtk_widget_get_allocated_height (GtkWidget *widget)
+{
+  GtkAllocation alloc = { 0, 0, 0, 0 };
+  g_return_val_if_fail (GTK_IS_WIDGET (widget), -1);
+  gtk_widget_get_allocation (widget, &alloc);
+  return alloc.height;
+}
+#endif
+
 #endif /* __MOUSEPAD_GTK_COMPAT_H__ */
